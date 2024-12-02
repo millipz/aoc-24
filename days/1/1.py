935d2f351a943dc617d68cc31c3b9cbb86016ee6
nc-milesphillips <184493025+nc-milesphillips@users.noreply.github.com>
2024-12-02 17:58:27 +0000

    Refactors

--- days/1/1.py
+++ days/1/1.py
@@ -1,15 +1,14 @@
-def aoc_1_1():
+def aoc_1_1() -> int:
     with open("days/1/input.txt", "r") as f:
         pairs = [nums.split() for nums in f.readlines()]
 
     list_a = [int(pair[0]) for pair in pairs]
     list_b = [int(pair[1]) for pair in pairs]
 
-    [list.sort() for list in [list_a, list_b]]
+    zipped = zip(sorted(list_a), sorted(list_b))
 
-    zipped = zip(list_a, list_b)
+    return sum([abs(pair[0] - pair[1]) for pair in zipped])
 
-    return sum([abs(pair[0]-pair[1]) for pair in zipped])
 
 def aoc_1_2():
     with open("days/1/input.txt", "r") as f:
@@ -21,6 +20,7 @@ def aoc_1_2():
     sim_scores = [num * list_b.count(num) for num in list_a]
 
     return sum(sim_scores)
+
 
 print(aoc_1_1())
 print(aoc_1_2())
--- days/2/2.py
+++ days/2/2.py
@@ -1,17 +1,20 @@
-def aoc_2_1(dampers = 0):
+def aoc_2_1(dampers: int = 0) -> int:
 
     def is_safe(report: list, lives: int = dampers) -> bool:
         direction = 1 if report[0] < report[1] else -1
         for i in range(1, len(report)):
-            diff = report[i]-report[i-1]
-            if abs(diff) not in range(1,4) or diff*direction < 0:
+            diff = report[i] - report[i - 1]
+            if abs(diff) not in range(1, 4) or diff * direction < 0:
                 lives -= 1
-        return lives >= 0
+                if lives < 0:
+                    return False
+        return True
 
     with open("days/2/input.txt", "r") as f:
         reports = [[int(num) for num in nums.split()] for nums in f.readlines()]
 
     return sum([is_safe(report) for report in reports])
+
 
 print(aoc_2_1())
 print(aoc_2_1(1))
